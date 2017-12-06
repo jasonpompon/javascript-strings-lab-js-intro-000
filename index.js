a4d1c77db791e8e01a19e4415a3b1a7c820f63a3
Jason Pompon <jasonpompon@gmail.com>
2017-12-06 11:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let\'s talk more about ${topic}`;
+var conversation = `Let\'s talk more about ${topic}.`;
